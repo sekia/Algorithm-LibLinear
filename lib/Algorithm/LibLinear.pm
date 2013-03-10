aca8c610e391f7604db356f7a9b1f2fce369c61c
Koichi SATOH <sato@seesaa.co.jp>
2013-03-10 12:49:34 +0900

    Work for releasing to CPAN. Add SYNOPSIS and copyright information.

--- lib/Algorithm/LibLinear.pm
+++ lib/Algorithm/LibLinear.pm
@@ -7,7 +7,7 @@ use Algorithm::LibLinear::Types;
 use Smart::Args;
 use XSLoader;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 XSLoader::load(__PACKAGE__, $VERSION);
 
@@ -142,15 +142,35 @@ __END__
 
 =head1 NAME
 
-Algorithm::LibLinear -
+Algorithm::LibLinear - A Perl binding for LIBLINEAR.
 
 =head1 SYNOPSIS
 
   use Algorithm::LibLinear;
+  # Constructs a model for L2-regularized L2 loss support vector classification.
+  my $learner = Algorithm::LibLinear->new(
+    cost => 1,
+    epsilon => 0.01,
+    solver => 'L2R_L2LOSS_SVC_DUAL',
+  );
+  # Loads a training data set from DATA filehandle.
+  my $data_set = Algorithm::LibLinear::DataSet->load(fh => \*DATA);
+  # Executes training.
+  my $classifier = $learner->train(data_set => $data_set);
+  # Determines which (+1 or -1) is the class for the given feature to belong.
+  my $class_label = $classifier->predict(feature => +{ 1 => 0.38, 2 => -0.5, ... });
+
+  __DATA__
+  +1 1:0.708333 2:1 3:1 4:-0.320755 5:-0.105023 6:-1 7:1 8:-0.419847 9:-1 10:-0.225806 12:1 13:-1 
+  -1 1:0.583333 2:-1 3:0.333333 4:-0.603774 5:1 6:-1 7:1 8:0.358779 9:-1 10:-0.483871 12:-1 13:1 
+  +1 1:0.166667 2:1 3:-0.333333 4:-0.433962 5:-0.383562 6:-1 7:-1 8:0.0687023 9:-1 10:-0.903226 11:-1 12:-1 13:1 
+  -1 1:0.458333 2:1 3:1 4:-0.358491 5:-0.374429 6:-1 7:-1 8:-0.480916 9:1 10:-0.935484 12:-0.333333 13:1 
+  -1 1:0.875 2:-1 3:-0.333333 4:-0.509434 5:-0.347032 6:-1 7:1 8:-0.236641 9:1 10:-0.935484 11:-1 12:-0.333333 13:-1 
+  ...
 
 =head1 DESCRIPTION
 
-Algorithm::LibLinear is
+Algorithm::LibLinear is a XS module that provides features of LIBLINEAR, a fast C library for classification and regression analysis.
 
 =head1 AUTHOR
 
@@ -158,9 +178,54 @@ Koichi SATOH E<lt>sato@seesaa.co.jpE<gt>
 
 =head1 SEE ALSO
 
+L<LIBLINEAR Homepage|http://www.csie.ntu.edu.tw/~cjlin/liblinear/>
+
+L<Algorithm::SVM> - A Perl binding to LIBSVM.
+
 =head1 LICENSE
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+=head2 Algorithm::LibLinear
+
+Copyright (c) 2013 Koichi SATOH. All rights reserved.
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ``AS IS'', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+=head2 LIBLINEAR
+
+Copyright (c) 2007-2013 The LIBLINEAR Project.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither name of copyright holders nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =cut
